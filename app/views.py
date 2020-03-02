7f1eb5a6535c111b3f3dd8170ea7653224f2c71e
Ubuntu <ubuntu@ip-172-31-19-39.ec2.internal>
2020-03-02 01:43:31 +0000

    added login and logout

--- app/views.py
+++ app/views.py
@@ -58,10 +58,12 @@ def login():
                 # get user id, load into session
                 login_user(user)
                 flash('Logged in successfully.', 'success')
+                return redirect(url_for('home'))
+
             else:
-                flash('Username or Password is incorrect.', 'danger')
+                flash('Username or Password is incorrect.')
                 # remember to flash a message to the user
-                return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
     
     return render_template("login.html", form=form)
 
